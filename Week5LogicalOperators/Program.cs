5cd9a1fb5f164f6490dff497fd52e9bf2ac3dc92
BearTheWhale <markuskaru6@gmail.com>
2023-12-14 21:37:06 +0200

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿// AND - &&
+//true && truse = true
+//true && false = false
+//false && true = false
+//false && false = false
+
+
+// OR - || (Pipes)
+// true || true - true
+// true || false - true
+// false || true - true
+// false || false - false
